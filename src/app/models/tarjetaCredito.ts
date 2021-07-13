ab9c274b7534de3d0830b097a659f626180e2a79
Johan Samper Gómez <johansamperg@gmail.com>
2021-07-13 13:02:02 -0500

    init

---
+++ src/app/models/tarjetaCredito.ts
@@ -0,0 +1,7 @@
+export class TarjetaCredito {
+    id?: number;
+    titular: string | undefined;
+    numeroTarjeta: string | undefined;
+    fechaExpiracion: string | undefined;
+    cvv: string | undefined;
+}
---
+++ src/app/services/tarjeta.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TarjetaCredito } from '../models/tarjetaCredito';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TarjetaService {
+  myAppUrl = 'https://localhost:44331/';
+  myApiUrl = 'api/TarjetaCredito/';
+  list = [];
+  constructor(private http: HttpClient) { }
+
+  guardarTarjeta(tarjeta: TarjetaCredito) : Observable<TarjetaCredito> {
+    return this.http.post<TarjetaCredito>(this.myAppUrl + this.myApiUrl, tarjeta)
+  }
+
+  obtenerTarjetas(){
+    this.http.get(this.myAppUrl+this.myApiUrl).toPromise().then(
+      data=>{
+        this.list = data;
+      });
+  }
+
+}
